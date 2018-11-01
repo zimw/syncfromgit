a9bb521ca723300f68f7f8b388b889c26b89a09f
Michael Wang <miwang@Michaels-MBP.fios-router.home>
2018-11-01 01:10:22 -0400

    bell triangle

--- atest.cpp
+++ atest.cpp
@@ -1,2 +1,21 @@
-include<string>
-int a = 0;
+int solution3(vector<int>& A, int X) {
+ int N = A.size();
+ if (N == 0) {
+  return -1;
+ }
+ int l = 0;
+ int r = N - 1;
+ while (l < r && A[l]!=X) {
+  int m = (l + r + 1) / 2;
+  if (A[m] > X) {
+   r = m - 1;
+  }
+  else {
+   l = m;
+  }
+ }
+ if (A[l] == X) {
+  return l;
+ }
+ return -1;
+}
